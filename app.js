2eb1350dbe247f0e7b548da051c2ca334b9a2de6
johnshumiston <johnshumiston@gmail.com>
2016-04-26 17:54:19 -0400

    nailed it

--- app.js
+++ app.js
@@ -1,10 +1,54 @@
 var express = require( 'express' );
 var app = express(); // creates an instance of an express application
 var port = 3000;
+var morgan = require("morgan");
+var swig = require("swig");
+var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+var routes = require('./routes/');
 
-app.get("/", function(request, response){
-	response.send("Bienvenidos");
-});
+
+app.use('/', routes);
+app.use(express.static('public'));
+
+
+
+// app.get("/stylesheets/style.css", function(req, res){
+// 	res.sendFile(__dirname + '/public/stylesheets/style.css');
+// });
+// var locals = {
+// 	title: 'An Example',
+//     people: [
+//         { name: 'Spock'},
+//         { name: 'Frodo' },
+//         { name: 'Hermione'}
+//     ]
+// };
+
+// swig.renderFile(__dirname + '/views/index.html', locals, function (err, output) {
+//     console.log(output);
+// });
+// swig.setDefaults({ cache: false });
+
+
+app.engine("html", swig.renderFile);
+app.set("view engine", "html");
+app.set("views", __dirname + '/views');
+
+
+// app.use(morgan("dev"));
+
+
+// app.get("/", function(req, res){
+// 	res.render( 'index', {title: 'Hall of Fame', people: locals.people} );
+// });
+
+// app.get("/is-anybody-in-there", function(request, response){
+// 	response.send("nah youre in this one alone");
+// });
+
+// app.post("/modernism", function(request, response){
+// 	response.send("Bienvenidos a the news");
+// });
 
 app.listen(port, function(){
 	console.log("We listening on port tree thousand");
